2e4f1f8aecff5ef750069db9a090800ea21b1bc4
YUKIJUDAI <1787149082@qq.com>
2020-03-23 10:02:58 +0800

    init

--- src/renderer/store/index.ts
+++ src/renderer/store/index.ts
@@ -7,14 +7,14 @@ import { createPersistedState } from "vuex-electron";
 Vue.use(Vuex);
 
 const state = {
-    userInfo: { }, // 用户信息
+    userInfo: {}, // 用户信息
     taobaoInfo: {}, // 淘宝信息
     menuInfo: [] // 菜单信息
 };
 
 const mutations = {
     SET_USER_INFO(state, obj) {
-        state.userInfo = Object.assign(state.userInfo, obj);
+        state.userInfo = obj;
     },
     SET_MENU_INFO(state, arr) {
         state.menuInfo = arr;
@@ -22,8 +22,12 @@ const mutations = {
 };
 
 const actions = {
-    set_user_info({ commit }, obj) {
-        commit("SET_USER_INFO", obj);
+    set_user_info({ commit, state }, obj) {
+        if (JSON.stringify(obj) === "{}") {
+            commit("SET_USER_INFO", {});
+        } else {
+            commit("SET_USER_INFO", Object.assign(obj, state.userInfo));
+        }
     },
     set_menu_info({ commit }, arr) {
         commit("SET_MENU_INFO", arr);
