3cb9da539faa8a0ba53180a8cc6863a8b0341d06
YUKIJUDAI <1787149082@qq.com>
2020-04-16 09:19:40 +0800

    init

--- src/renderer/util/factory.ts
+++ src/renderer/util/factory.ts
@@ -1,8 +1,24 @@
 const { from } = require("rxjs");
+var CryptoJS = require("crypto-js");
+var AES = require("crypto-js/aes");
 
-const { aes } = require("./aes");
 import http from "./http";
 import { getGlobal, wd, getAllWindows, fromId } from "./electronFun";
+
+const aes = function(txt) {
+    var key = "w28Cz694s63kBYk4";
+    var io = "4kYBk36s496zC82w";
+
+    var txt2 = CryptoJS.enc.Base64.stringify(CryptoJS.enc.Hex.parse(txt));
+
+    var res = AES.decrypt(txt2, CryptoJS.enc.Utf8.parse(key), {
+        iv: CryptoJS.enc.Utf8.parse(io),
+        mode: CryptoJS.mode.CBC,
+        padding: CryptoJS.pad.Pkcs7,
+    });
+
+    return res.toString(CryptoJS.enc.Utf8);
+};
 
 function Factory() {
     this.obj = {};
