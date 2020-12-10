f733b9138998095854ef29a6263e823d46528be4
YUKIJUDAI <1787149082@qq.com>
2020-12-10 16:15:18 +0800

    init

--- src/renderer/config/config.ts
+++ src/renderer/config/config.ts
@@ -1,10 +1,10 @@
 const config = require("@@/package.json");
 
 export default {
-    baseUrl: "http://mars-api.huoxingqingbao.com",
-    heisouBaseUrl: "http://mars-hesso.huoxingqingbao.com",
-    qnUrl: "http://file.bsearch.lethink.net/",
-    wsUrl: "ws://47.99.101.142:2046/",
+    baseUrl: "",
+    heisouBaseUrl: "",
+    qnUrl: "",
+    wsUrl: "",
     proxyid: config.build.publish[0].url.split("/")[config.build.publish[0].url.split("/").length - 1],
     version_num: config.version,
 };
